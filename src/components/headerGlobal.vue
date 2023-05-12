4bc16a1869fd65a451f867dde837271f42c5490d
Gustavo Oliveira <gustaoliveiradev@gmail.com>
2023-05-12 15:03:27 -0400

    arrumando o menu

--- src/components/headerGlobal.vue
+++ src/components/headerGlobal.vue
@@ -3,54 +3,31 @@
   <div class="navbar-container">
 
     <img class="imgharder"
-    src="../../public/direotias/douglascosta_82934045_116411013185365_8270789263511275541_n_widelg.jpg" alt="logo">
-    <div class="navbar">
-      <input type="checkbox" name="" id="" />
+      src="../../public/direotias/douglascosta_82934045_116411013185365_8270789263511275541_n_widelg.jpg" alt="logo">
 
-
-      <div class="hamburger-lines">
-        <span class="line line1"></span>
-        <span class="line line2"></span>
-        <span class="line line3"></span>
-      </div>
-
-
-      <div class="menu-items">
-        <router-link class="rotas" :to="{ name: 'Home' }">Home</router-link>
-        <router-link class="rotas" :to="{ name: 'Atuacao' }">Atuação</router-link>
-        <router-link class="rotas" :to="{ name: 'Contato' }">Contato</router-link>
-        <router-link class="rotas" :to="{ name: 'SobreNós' }">Sobre nós</router-link>
-      
-      </div>
-      <router-view />
+    <div class="menuWeb">
+      <router-link class="rotas" :to="{ name: 'Home' }">Home</router-link>
+      <router-link class="rotas" :to="{ name: 'Atuacao' }">Atuação</router-link>
+      <router-link class="rotas" :to="{ name: 'Contato' }">Contato</router-link>
+      <router-link class="rotas" :to="{ name: 'SobreNós' }">Sobre nós</router-link>
     </div>
 
-  
-  </div>
+    <div class="menuMobile">
+      <toogleMenuVue />
+    </div>
 
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
-
+import toogleMenuVue from './toogleMenu.vue'
 
 export default {
   components: {
+    toogleMenuVue,
+  },
 
-  },
-  setup() {
-    let toggleMenu = ref(true)
-    // const route = useRoute();
-    return {
-      toggleMenu
-    }
-  },
-  methods: {
-    Sidebar() {
-      this.toggleMenu = !this.toggleMenu
-    }
-  }
+
 }
 </script>
 
@@ -60,197 +37,46 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-.imgharder{
+
+.navbar-container {
+  width: 100vw;
+  display: flex;
+  align-items: center;
+
+  background: #4F4F4F;
+  margin-bottom: 50px;
+}
+
+
+
+.menuMobile {
+  display: none;
+}
+
+.imgharder {
   height: 100px;
   width: 150px;
   margin-left: 25px;
 }
 
-.navbar-container {
-  width: 100vw;
 
-  display: flex;
-  align-items: center;
-  background: 	#4F4F4F;
-  margin-bottom: 50px;
-}
+@media (max-width: 500px) {
 
-.navbar {
-  height: 100px;
-  width: 100vw;
-  display: flex;
-  margin-right: 25px;
-  align-items: center;
-  justify-content: end;
-}
-
-.navbar-container input[type="checkbox"],
-.navbar-container .hamburger-lines {
-  display: none;
-}
-
-.menu-items {
-  visibility: visible;
-}
-
-.menu-items {
-  display: flex;
-  align-items: center;
-  font-weight: 500;
-}
-
-.items a {
-  margin-bottom: 15px;
-  margin-right: 15px;
-  text-decoration: none;
-  color: white;
-}
-.rotas{
-  cursor: pointer;
- 
-  margin-right: 15px;
-  text-decoration: none;
-  color: white;
-}
-
-.items a :hover {
-  color: #dcb675;
-  transition: 300ms;
-}
-
-.rotas.active a {
-  color: #dcb675;
-  transition: 300ms;
-}
-
-
-.lupa {
-  margin-right: 20px;
-}
-
-.magnify {
-  display: flex;
-  align-items: center;
-  height: 100px;
-  color: white;
-}
-
-
-@media (max-width: 768px) {
-
-  .navbar input[type="checkbox"],
-  .navbar .hamburger-lines {
-    display: block;
-  }
-
-  .imgharder{
+  .imgharder {
     position: absolute;
     right: 20px;
-
-    margin:0;
+    margin: 0;
     padding: 5px;
     width: 100px;
     height: 70px;
-  
-  
-
-
-}
+  }
 
   .navbar-container {
     height: 60px;
     width: 100vw;
   }
-  .navbar {
-    display: block;
-    position: relative;
-    height: 50px;
-    width: 100vw;
-  }
 
 
-  .navbar input[type="checkbox"] {
-    position: absolute;
-    display: block;
-    height: 32px;
-    width: 30px;
-    top: 20px;
-    left: 20px;
-    z-index: 5;
-    opacity: 0;
-  }
 
-  .navbar .hamburger-lines {
-    display: block;
-    height: 23px;
-    width: 35px;
-    position: absolute;
-    top: 17px;
-    left: 20px;
-    z-index: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  .navbar .hamburger-lines .line {
-    display: block;
-    height: 4px;
-    border-radius: 10px;
-    background: white;
-  }
-
-  .navbar .hamburger-lines .line1 {
-    transform-origin: 0% 0%;
-    transition: transform 0.4s ease-in-out;
-  }
-
-  .navbar .hamburger-lines .line2 {
-    transition: transform 0.2s ease-in-out;
-  }
-
-  .navbar .hamburger-lines .line3 {
-    transform-origin: 0% 100%;
-    transition: transform 0.4s ease-in-out;
-  }
-
-  .navbar .menu-items {
-    padding-top: 100px;
-    background: 	#4F4F4F;
-    height: 100vh;
-    max-width: 300px;
-    transform: translate(-150%);
-    display: flex;
-    flex-direction: column;
-    margin-left: -40px;
-    padding-left: 50px;
-    transition: transform 0.5s ease-in-out;
-  }
-
-  .items {
-    margin-bottom: 15px;
-  }
-
-  .navbar .menu-items {
-    margin-bottom: 1.5rem;
-    font-size: 1.3rem;
-    font-weight: 500;
-  }
-
-  .navbar input[type="checkbox"]:checked~.menu-items {
-    transform: translateX(0);
-  }
-
-  .navbar input[type="checkbox"]:checked~.hamburger-lines .line1 {
-    transform: rotate(35deg);
-  }
-
-  .navbar input[type="checkbox"]:checked~.hamburger-lines .line2 {
-    transform: scaleY(0);
-  }
-
-  .navbar input[type="checkbox"]:checked~.hamburger-lines .line3 {
-    transform: rotate(-35deg);
-  }
 }
 </style>
---
+++ src/components/toogleMenu.vue
@@ -0,0 +1,153 @@
+<template>
+    <div id="menu">
+        <div ref="menuBar" id="menuBar" @click="toggleMenu()">
+            <div id="bar1" class="bar"></div>
+            <div id="bar2" class="bar"></div>
+            <div id="bar3" class="bar"></div>
+        </div>
+        <nav ref="nav"  class="nav" id="nav">
+            
+            <!-- <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul> -->
+
+            <router-link class="rotas" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="rotas" :to="{ name: 'Atuacao' }">Atuação</router-link>
+            <router-link class="rotas" :to="{ name: 'Contato' }">Contato</router-link>
+            <router-link class="rotas" :to="{ name: 'SobreNós' }">Sobre nós</router-link>
+        </nav>
+    </div>
+
+    <div ref="menuBg"  class="menuBg" id="menuBg"></div>
+
+
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+
+    setup() {
+        return {
+            isMenuOpen: ref(false)
+        }
+    },
+
+    methods: {
+    
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+            console.log(this.isMenuOpen)
+            if (this.isMenuOpen === true) {
+                    this.$refs.menuBar.classList.add('change');
+                    this.$refs.nav.classList.add('change');
+                    this.$refs.menuBg.classList.add('change-bg');
+            } else{
+                this.$refs.menuBar.classList.remove('change');
+                    this.$refs.nav.classList.remove('change');
+                    this.$refs.menuBg.classList.remove('change-bg');
+            }
+        }
+    }
+}
+</script>
+
+<style>
+#menu {
+    z-index: 2;
+}
+
+#menuBar {
+    width: 45px;
+    height: 40px;
+    margin: 30px 0 20px 20px;
+    cursor: pointer;
+}
+
+.bar {
+    height: 5px;
+    width: 100%;
+    background-color: white;
+    display: block;
+    border-radius: 5px;
+    transition: 0.3s ease;
+}
+
+#bar1 {
+    transform: translateY(-4px);
+}
+
+#bar3 {
+    transform: translateY(4px);
+}
+
+.nav {
+    transition: 0.3s ease;
+    display: none;
+}
+
+.nav ul {
+    padding: 0 22px;
+}
+
+.nav li {
+    list-style: none;
+    padding: 12px 0;
+}
+
+.nav li a {
+    color: white;
+    font-size: 20px;
+    text-decoration: none;
+}
+
+.nav li a:hover {
+    font-weight: bold;
+}
+
+.menuBg,
+#menu {
+    top: 0;
+    left: 0;
+    position: absolute;
+}
+
+.menuBg {
+    z-index: 1;
+    width: 0;
+    height: 0;
+    margin: 30px 0 20px 20px;
+    background: radial-gradient(circle, #4F4F4F, #4F4F4F);
+    border-radius: 50%;
+    transition: 0.3s ease;
+}
+
+.change {
+    display: block;
+}
+
+.change .bar {
+    background-color: white;
+}
+
+.change #bar1 {
+    transform: translateY(4px) rotateZ(-45deg);
+}
+
+.change #bar2 {
+    opacity: 0;
+}
+
+.change #bar3 {
+    transform: translateY(-6px) rotateZ(45deg);
+}
+
+.change-bg {
+    width: 520px;
+    height: 460px;
+    transform: translate(-60%, -30%);
+}
+</style>
